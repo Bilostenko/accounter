5b643d78aa0110b5ff91771af7aeaa0f58f132f8
Bilostenko <bilostenkod@gmail.com>
2023-02-14 11:51:03 +0200

    fix js

--- employers.js
+++ employers.js
@@ -1,12 +1,12 @@
 const array = [];
-const num = +prompt('Enter the number of employees');
+const num = +prompt('Number of employees');
 
 for (let i = 1; i <= num; i++) {
     array[i] = {
-        firstName: prompt('Enter First Name ' + i + ' employee'),
-        lastName: prompt('Enter Last Name ' + i + ' employee'),
-        age: +prompt('Enter age ' + i + ' employee'),
-        occupation: prompt('Enter occupation' + i + ' employee'),
+        firstName: prompt('First Name ' + i + ' employee'),
+        lastName: prompt('Last Name ' + i + ' employee'),
+        age: +prompt('Age ' + i + ' employee'),
+        occupation: prompt('Occupation' + i + ' employee'),
         show: function () {
             document.write('Employee ' + i + ": " + this.firstName + ', ' + this.lastName + ', ' + this.age + ', ' + this.occupation + "." + '<br>');
         }
