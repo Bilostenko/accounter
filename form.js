92d94e96b47eed73f961c832aebdfc8a2af198a2
Bilostenko <bilostenkod@gmail.com>
2023-02-14 11:49:11 +0200

    fix employers

--- form.js
+++ form.js
@@ -21,17 +21,17 @@ persAddress.addEventListener('input', personalAddress)
 
 
 /* prevent entering negative numbers */
-debt.onkeydown = function(e) {
-    if(!((e.keyCode > 95 && e.keyCode < 106)
-      || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
-        return false;
-    }
+debt.onkeydown = function (e) {
+  if (!((e.keyCode > 95 && e.keyCode < 106)
+    || (e.keyCode > 47 && e.keyCode < 58)
+    || e.keyCode == 8)) {
+    return false;
+  }
 }
 /* prevent entering numbers in text input*/
-persData.onkeydown = function(e) {
+persData.onkeydown = function (e) {
   var key = e.keyCode;
-  return ((key >= 65 && key <= 90) || key == 8 ||  key == 32);
+  return ((key >= 65 && key <= 90) || key == 8 || key == 32);
 };
 
 function payment() {
@@ -39,28 +39,29 @@ function payment() {
   if (property.checked) {
     if ((debt.value * 0.02) <= (minSalary * 10)) {
       cost.innerHTML = (debt.value * 0.02).toFixed(2) + " грн"
-      localStorage.setItem('cost',((debt.value * 0.02).toFixed(2)))
+      localStorage.setItem('cost', ((debt.value * 0.02).toFixed(2)))
     } else if ((debt.value * 0.02) > (minSalary * 10)) {
       cost.innerHTML = (minSalary * 10) + " грн"
-      localStorage.setItem('cost',(minSalary * 10))
+      localStorage.setItem('cost', (minSalary * 10))
     }
   } else if (nonProperty.checked) {
     if (physical.checked) {
       cost.innerHTML = minSalary + " грн"
-      localStorage.setItem('cost',minSalary)
+      localStorage.setItem('cost', minSalary)
     } else if (judical.checked) {
       cost.innerHTML = minSalary * 2 + " грн"
-      localStorage.setItem('cost',(minSalary * 2))
+      localStorage.setItem('cost', (minSalary * 2))
     }
-  }  
+  }
 }
 
-function personalData(){
-localStorage.setItem('personalData', persData.value)
+function personalData() {
+  localStorage.setItem('personalData', persData.value)
 }
-function personalAddress(){
-localStorage.setItem('personalAddress', persAddress.value)
+function personalAddress() {
+  localStorage.setItem('personalAddress', persAddress.value)
 }
+
 
 
 
